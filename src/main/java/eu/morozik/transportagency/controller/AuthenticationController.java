f88835924aaa22d9c8c0776dc3c3cc19a2af3f4d
Yan Moro <yan.morozik24@mail.ru>
2022-05-04 16:36:51 +0300

    completed security

--- src/main/java/eu/morozik/transportagency/controller/AuthenticationController.java
+++ src/main/java/eu/morozik/transportagency/controller/AuthenticationController.java
@@ -1,7 +1,15 @@
 package eu.morozik.transportagency.controller;
 
+import eu.morozik.transportagency.api.dao.UserDao;
 import eu.morozik.transportagency.dto.AuthenticationDto;
+import eu.morozik.transportagency.model.User;
+import eu.morozik.transportagency.security.JwtTokenProvider;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,14 +18,37 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthenticationController {
 
+    private final AuthenticationManager authenticationManager;
+    private UserDao userDao;
+    private JwtTokenProvider jwtTokenProvider;
+
+    public AuthenticationController(AuthenticationManager authenticationManager, UserDao userDao, JwtTokenProvider jwtTokenProvider) {
+        this.authenticationManager = authenticationManager;
+        this.userDao = userDao;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
     @PostMapping("/login")
     public ResponseEntity<?> authenticate(@RequestBody AuthenticationDto request) {
-        return null;
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+            User user = userDao.findByEmail(request.getEmail()).orElseThrow(() -> new UsernameNotFoundException("User doesn't exists"));
+            String token = jwtTokenProvider.creatToken(request.getEmail(), user.getRole().name());
+            Map<Object, Object> response = new HashMap<>();
+            response.put("email", request.getEmail());
+            response.put("token", token);
+
+            return ResponseEntity.ok(response);
+        } catch (AuthenticationException e) {
+            return new ResponseEntity<>("Invalid email/password combination", HttpStatus.FORBIDDEN);
+        }
     }
 
     @PostMapping("/logout")
